31020aa7284f1a88ef3fe88c3dd5d4d9f3255770
soori <soori@LAPTOP-GHCIP61N>
2023-01-18 11:16:33 +0530

    Commit

---
+++ src/test/java/demo/Student.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Student {
+	@Test
+	public void demo()
+	{
+		System.out.println("hi");
+	}
+	@Test
+	public void demo1()
+	{
+		System.out.println("hello");
+	}
+
+}
