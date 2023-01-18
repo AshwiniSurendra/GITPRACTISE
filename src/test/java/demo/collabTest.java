087aa32d401e57fd01e3d9bf39864bd91710f2f5
sharunbansal04 <sharun.bansal00@gmail.com>
2023-01-18 13:32:24 +0530

    commit by collaborator

---
+++ src/test/java/demo/collabTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class collabTest {
+	
+	@Test
+	public void collaborationTest() {
+		System.out.println("class added by Sharun");
+	}
+
+}
