e608be0fb2013f96b868e6ad790fe6d8f116d726
soori <soori@LAPTOP-GHCIP61N>
2023-01-19 11:12:09 +0530

    commit by ashwini

--- src/test/java/demo/Student.java
+++ src/test/java/demo/Student.java
@@ -4,14 +4,19 @@ import org.testng.annotations.Test;
 
 public class Student {
 	@Test
-	public void demo()
+	public void test()
+	{
+		System.out.println("this is test");
+	}
+	@Test
+	public void demo2()
 	{
 		System.out.println("hi");
 	}
 	@Test
 	public void demo1()
 	{
-		System.out.println("hello");
+		System.out.println("hai hello ello");
 	}
 
 }
