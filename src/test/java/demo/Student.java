5bd6e67431a3fac654ad47e4bd3bbcba5a9ebedc
soori <soori@LAPTOP-GHCIP61N>
2023-01-19 19:03:49 +0530

    commit at 7

--- src/test/java/demo/Student.java
+++ src/test/java/demo/Student.java
@@ -22,15 +22,5 @@ public class Student {
 	{
 		System.out.println("this is practice");
 	}
-	@Test
-	public void demo2()
-	{
-		System.out.println("hi");
-	}
-	@Test
-	public void demo1()
-	{
-		System.out.println("hai hello ello");
-	}
-
+	
 }
